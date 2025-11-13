6cf0bfc66d34de833b39f1eb993939df5de3da6b
852ftw <contaxuxu07@gmail.com>
2025-11-13 19:55:19 -0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -2,7 +2,7 @@
 local HttpService = game:GetService("HttpService")
 local request = request or http_request or (syn and syn.request)
 
-local API_URL = "https://SEU_REPLIT.repl.co/api/validate"
+local API_URL = "https://replit.com/@contaxuxu07/RomelKeySystem"
 local KEY = _G.KEY
 
 local res = request({Url = API_URL .. "?key=" .. KEY, Method = "GET"})
